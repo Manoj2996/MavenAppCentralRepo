c8462b38356fd1c19c0f67c36e5fc832c8a66fc9
manojrk <manojrk@LIN20003541.corp.capgemini.com>
2020-09-30 16:32:06 +0530

    Build came success

---
+++ MavenRemoteRepo/src/main/java/com/cg/project/sonarTest.java
@@ -0,0 +1,12 @@
+package com.cg.project;
+
+public class sonarTest {
+public static void main(String[] args)
+{
+	System.out.println("helloworld");
+	int i=10;
+	int s=0;
+	s+=i;
+	System.out.println(s);
+}
+}
--- MavenRemoteRepo/src/test/java/com/cg/project/Apptest.java
+++ MavenRemoteRepo/src/test/java/com/cg/project/Apptest.java
@@ -1,5 +1,26 @@
 package com.cg.project;
 
+import org.junit.Test;
+
 public class Apptest {
+
+	
+    
+	@Test
+	public void test1() {
+	System.out.println("test1");
+	}
+	@Test
+	public void test2() {
+	System.out.println("test2");
+	}
+	@Test
+	public void test3() {
+	System.out.println("test3");
+	}
+	@Test
+	public void test4() {
+	System.out.println("test4");
+	}
 
 }
